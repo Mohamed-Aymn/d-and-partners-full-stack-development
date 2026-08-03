0cb4014f13caff978c569ecc8a08124fbee768c5
Mohamed Aymn <mohamedaymn218@gmail.com>
2026-08-03 19:12:14 +0300

    feat: added the third challenge

---
+++ 12/src/pages/Challenge3.tsx
@@ -0,0 +1,29 @@
+/**
+ * 
+ * # Challenge Description:
+ * increment the number when the button is clicked
+ * 
+ * # Acceptance Criteria:
+ * 1. When you run the app and navigate to /challenge3, you should 
+ *    see the number increment when the button is clicked.
+ * 2. Must be dong using a react hook.
+ * 3. use a child component to display the number.
+ * 
+ */
+
+function Challenge3Child() {
+  return (
+    <div>0</div>
+  )
+}
+
+export default function Challenge3() {
+  return (
+    <div>
+      <button className="border-2 border-black p-1">+</button>
+      <Challenge3Child />
+    </div>
+  )
+}
+
+
