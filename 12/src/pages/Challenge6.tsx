3efc488d5e1851cd62d46fbec97dd282dc2c3f6c
Mohamed Aymn <mohamedaymn218@gmail.com>
2026-08-03 22:17:06 +0300

    feat: added the 6th and 7th challenge

---
+++ 12/src/pages/Challenge6.tsx
@@ -0,0 +1,27 @@
+/**
+ * 
+ * # Challenge Description:
+ * render the data that exists in the list.
+ * 
+ * # Acceptance Criteria:
+ * 1. When you run the app and navigate to /challenge6, 
+ * 2. Render these items in an unordered list
+ * 
+ */
+
+export default function Challenge6() {
+  const data = [
+    "this is the first line",
+    "this is the second line",
+    "this is the third line",
+    "this is the fourth line",
+    "this is the fifth line",
+  ]
+
+  return (
+    <div>
+      {/* render items here */}
+    </div>
+  )
+}
+
---
+++ 12/src/pages/Challenge7.tsx
@@ -0,0 +1,44 @@
+/**
+ * 
+ * # Challenge Description:
+ * render the data that exists in the list.
+ * 
+ * # Acceptance Criteria:
+ * 1. When you run the app and navigate to /challenge7, 
+ * 2. Use the Item component.
+ * 
+ */
+
+
+function Item() {
+  return (
+    <div>
+      <div>title must be rendered here</div>
+      <p>description must be rendered here</p>
+    </div>
+  )
+}
+
+export default function Challenge7() {
+  const data = [
+    {
+      title: "this is the first line",
+      description: "this is the first line description"
+    },
+    {
+      title: "this is the second line",
+      description: "this is the second line description"
+    },
+    {
+      title: "this is the third line",
+      description: "this is the third line description"
+    },
+  ]
+
+  return (
+    <div>
+      {/* render items here */}
+    </div>
+  )
+}
+
